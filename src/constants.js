1fe2b0d2af23d98bf5e7af95600b2d717d5f53ac
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-02-21 23:21:13 -0500

    Widget Change 5.0

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://wbdv-generic-server.herokuapp.com/api/001059598/courses"
 export const DEFAULT_COURSE_TITLE = "New Course"
 export const DEFAULT_SECTION_TITLE = "New Section"
-export const WIDGET_SERVICE_URL="http://localhost:8080"
+export const WIDGET_SERVICE_URL="https://shielded-temple-70812.herokuapp.com"
