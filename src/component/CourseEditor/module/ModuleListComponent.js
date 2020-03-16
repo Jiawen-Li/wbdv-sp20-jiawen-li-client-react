ebe8b2fd9b9b1472d77c0bb3544b892fab3f9f42
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-16 18:52:15 -0400

    module list new 2.0

--- src/component/CourseEditor/module/ModuleListComponent.js
+++ src/component/CourseEditor/module/ModuleListComponent.js
@@ -12,56 +12,48 @@ class ModuleListComponent extends React.Component {
 
     render() {
         return (
-            <div class={'container'}>
-                <div class={'col-5'}>
-                    <div class={'row'}>
-                        {this.props.modules && this.props.modules.map((module, index) =>
-                            <div key={module._id}>
-                                <button onClick={
-                                    () => this.props.deleteModule(module._id)}>
-                                    <i className="fas fa-trash-alt"></i>
-                                </button>
-                                {index !== this.props.ifModuleEditingIndex &&
-                                <>
-                                    <button onClick={
-                                        () => this.props.editModule(index, module.title)}>
-                                        <i className="fas fa-pencil-alt"></i>
-                                    </button>
-                                    <Link to={`/course-editor/${this.props.match.params.courseId}/module/${module._id}`}>
-                                        {module.title}
-                                    </Link>
-                                </>}
-
-                                {index == this.props.ifModuleEditingIndex &&
-                                <>
-                                    <button onClick={
-                                        () =>
-                                            this.props.saveModule(
-                                                module._id,
-                                                {"title":this.props.moduleEditingContent}
-                                            )
-                                    }>
-                                        save
-                                    </button>
-                                    <input value={this.props.moduleEditingContent}
-                                           onChange={event => this.props.changeModule(event.target.value)}/>
-                                </>
-                                }
-                            </div>
-                        )}
-                    </div>
-                </div>
-
-                <div class={'col-6'}>
-                    <div class={'row justify-content-md-center'}>
+            <div class={'list-group'}>
+                {this.props.modules && this.props.modules.map((module, index) =>
+                    <li class="list-group-item" key={module._id}>
                         <button onClick={
-                            () => this.props.createModule(this.props.courseId)}>
-                            <i className="fas fa-plus"></i>
+                            () => this.props.deleteModule(module._id)}>
+                            <i className="fas fa-trash-alt"/>
                         </button>
-                    </div>
-                </div>
+
+                        {index !== this.props.ifModuleEditingIndex &&
+                        <>
+                            <button onClick={
+                                () => this.props.editModule(index, module.title)}>
+                                <i className="fas fa-pencil-alt"></i>
+                            </button>
+                            <Link to={`/course-editor/${this.props.match.params.courseId}/module/${module._id}`}>
+                                {module.title}
+                            </Link>
+                        </>}
+
+                        {index == this.props.ifModuleEditingIndex &&
+                        <>
+                            <button onClick={
+                                () =>
+                                    this.props.saveModule(module._id,
+                                        {"title": this.props.moduleEditingContent}
+                                    )
+                            }>
+                                save
+                            </button>
+                            <input value={this.props.moduleEditingContent}
+                                   onChange={event => this.props.changeModule(event.target.value)}/>
+                        </>}
+                    </li>)
+                }
+                <li class={'list-group-item'}>
+                    <button onClick={
+                        () => this.props.createModule(this.props.courseId)}>
+                        <i className="fas fa-plus"></i>
+                    </button>
+                </li>
             </div>
-        );
+        )
     }
 }
 
@@ -72,6 +64,7 @@ const stateToPropertyMapper = (state) => {
         moduleEditingContent: state.modules.moduleEditingContent
     }
 }
+
 
 const dispatchToPropertyMapper = (dispatch) => {
     return {
@@ -107,5 +100,5 @@ const dispatchToPropertyMapper = (dispatch) => {
 
 export default connect(
     stateToPropertyMapper,
-    dispatchToPropertyMapper)
-(ModuleListComponent)
+    dispatchToPropertyMapper)(ModuleListComponent)
+
