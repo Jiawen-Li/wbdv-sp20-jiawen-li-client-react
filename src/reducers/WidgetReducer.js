e531eb9ab0633ab060c8fc1d068160571a0843e5
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-19 00:11:15 -0400

    WidgetReducer.js

--- src/reducers/WidgetReducer.js
+++ src/reducers/WidgetReducer.js
@@ -1,13 +1,18 @@
 import {
+    CHANGE_WIDGET_EDITING_CONTENT,
+    CHANGE_WIDGET_EDITING_STATUS,
     CREATE_WIDGET,
     DELETE_WIDGET,
+    SAVE_WIDGET,
     UPDATE_WIDGET,
     FIND_ALL_WIDGETS_FOR_TOPIC
 } from "../actions/widgetActions";
 
 
 const initialState = {
-    widgets: []
+    widgets: [],
+    ifWidgetEditingIndex: -1,
+    WidgetEditingContent: ""
 }
 
 
@@ -16,6 +21,7 @@ const widgetReducer = (state = initialState, action) => {
         // TODO: use constants and actions
         case CREATE_WIDGET:
             return {
+                ...state,
                 widgets: [
                     ...state.widgets,
                     action.newWidget
@@ -43,6 +49,26 @@ const widgetReducer = (state = initialState, action) => {
             return {
                 ...state,
                 widgets: action.widgets
+            };
+
+        case CHANGE_WIDGET_EDITING_STATUS:
+            return {
+                ...state,
+                ifWidgetEditingIndex: action.index,
+                WidgetEditingContent: action.editContent
+            };
+
+        case CHANGE_WIDGET_EDITING_CONTENT:
+            return {
+                ...state,
+                WidgetEditingContent: action.editContent
+            };
+
+        case SAVE_WIDGET:
+            return {
+                ...state,
+                ifWidgetEditingIndex: -1,
+                WidgetEditingContent: ""
             };
 
         default:
