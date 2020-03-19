54f29f3d15400b65617a3d6307cc119a5a3c77f0
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-18 21:23:55 -0400

    ParagraphWidget refine

--- src/component/CourseEditor/Widgets/ParagraphWidget.js
+++ src/component/CourseEditor/Widgets/ParagraphWidget.js
@@ -1,59 +1,70 @@
-import React from "react";
+import React, {AllHTMLAttributes as widget} from "react";
 import {updateWidget} from "../../../actions/widgetActions";
 import {connect} from "react-redux";
 
 class ParagraphWidget extends React.Component {
+
+    editingView = () =>
+        <>
+            <span><h4>Paragraph Widget</h4></span>
+
+            <div className={'row'}>
+
+                <button><i className="fas fa-arrow-up"></i></button>
+                <button><i className="fas fa-arrow-down"></i></button>
+
+                <select value={this.props.widget.type}
+                        placeholder={'Paragraph'}
+                        onChange={event => this.props.updateWidget(this.props.widget.id, {
+                            ...this.props.widget,
+                            type: event.target.value
+                        })}>
+                    <option value={'HEADING'}>Heading</option>
+                    <option value={'PARAGRAPH'}>Paragraph</option>
+                    <option value={'LIST'}>List</option>
+                    <option value={'IMAGE'}>Image</option>
+                </select>
+
+                <button onChange={event => this.props.deleteWidget(this.props.widget.id)}>
+                    <i className="fas fa-times"></i>
+                </button>
+
+            </div>
+
+            <textarea className={'row'}
+                      style={{'margin-bottom': '10px', 'margin-top': '10px'}}
+                      value={this.props.widget.value}
+                      onChange={event => this.props.updateWidget(this.props.widget.id, {
+                          ...this.props.widget,
+                          value: event.target.value
+                      })}>
+                    </textarea>
+
+            <div className={'row'} style={{'margin-bottom': '10px'}}>
+                <input placeholder={'Widget name'} style={{'width': '600px'}}
+                       value={this.props.widget.name}
+                       onChange={event => this.props.updateWidget(this.widget.id,{
+                           ...this.props.widget,
+                           name: event.target.value
+                           }
+                       )}/>
+            </div>
+
+        </>
+
+    preview = () =>
+        <div>
+            <h1>Preview</h1>
+            <h3>{widget.value}</h3>
+        </div>
+
     render() {
         return (
             <li className={'list-group-item'}>
-                <span><h4>Paragraph Widget</h4></span>
-                <div className={'row'}>
-                    <button><i className="fas fa-arrow-up"></i></button>
-                    <button><i className="fas fa-arrow-down"></i></button>
-                    <select value={'Paragraph'}>
-                        <option value={'HEADING'}>Heading</option>
-                        <option value={'PARAGRAPH'}>Paragraph</option>
-                        <option value={'LIST'}>List</option>
-                        <option value={'IMAGE'}>Image</option>
-                    </select>
-                    <button><i className="fas fa-times"></i></button>
-                </div>
-                <textarea className={'row'}
-                          style={{'margin-bottom': '10px', 'margin-top': '10px'}}
-                          value={'Lorem ipsum'}>
-                    </textarea>
-                <div className={'row'} style={{'margin-bottom': '10px'}}>
-                    <input value={'Widget name'} style={{'width': '600px'}}/>
-                </div>
-                <div className={'row'} style={{'margin-bottom': '10px'}}>
-                    <h4>Preview</h4>
-                </div>
-                <div className={'row'} style={{'margin-bottom': '10px'}}>
-                    <h3>Lorem ipsum</h3>
-                </div>
+                {this.editingView()}
+                {!this.props.editing && this.preview()}
             </li>
 
-            // <div>
-            //     {
-            //         this.props.editing &&
-            //         <textarea
-            //             value={this.props.widget.value}
-            //             onChange={(e) => {
-            //                 this.props.updateWidget(this.props.widget.id, {
-            //                     ...this.props.widget,
-            //                     value: e.target.value
-            //                 })
-            //             }
-            //             }>
-            //         </textarea>
-            //     }
-            //     {
-            //         !this.props.editing &&
-            //         <span>
-            //             {this.props.widget.value}
-            //         </span>
-            //     }
-            // </div>
         )
     }
 }
